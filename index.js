b66233b0352bae8b9ae228c63173696b03522129
Shubham <123shubhamraut@gmail.com>
2020-07-23 14:39:59 +0530

    modified according to ES6

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
-$(document).ready(function () {
-  var results = [],
-    username = '',
-    selectedAnswers = {};
+$(document).ready(() => {
+  let username = "";
+  let results = [];
+  let selectedAnswers = {};
 
-  //dom elements
-  var quizContainer = $("#quiz-container");
-  var usernameInput = document.getElementById('username');
-  var modal = document.getElementById('modal-container');
-  // var quizContainer = document.getElementById('quiz-container');
-  var btnGroup = document.querySelector('.buttons');
-  var submitBtn = document.querySelector('#submit');
-  var resetBtn = document.querySelector('#reset');
+  // Cache the dom elems
+  const usernameInput = $("#username");
+  const modal = $("#modal-container");
+  const quizContainer = $("#quiz-container");
+  const submitBtn = $("#submit");
+  const resetBtn = $("#reset");
+  const btnGroup = $(".buttons");
 
-  // on clicking the ans
-  function onAnswerClick(e) {
+  // const usernameInput = document.getElementById('username');
+  // const modal = document.getElementById('modal-container');
+  // // const quizContainer = document.getElementById('quiz-container');
+  // const btnGroup = document.querySelector('.buttons');
+  // const submitBtn = document.querySelector('#submit');
+  // const resetBtn = document.querySelector('#reset');
+
+  // On clicking the quiz answer
+  const onAnswerClick = (e) => {
     e.stopPropagation();
-    var answerEl = $(e.target);
-    var index = answerEl
+
+    const answerEl = $(e.target);
+    const index = answerEl
       .parent()
       .parent()
       .attr("data-index");
-    // var index=e.target.parentNode.parentNode.getAttribute('data-index');
+    // const index=e.target.parentNode.parentNode.getAttribute('data-index');
 
-    var answer = answerEl.text();
-    // var answer=e.target.innerText;
-    var isCorrect = results[index].correct_answer === answer;
+    const answer = answerEl.text();
+    // const answer=e.target.innerText;
+    const isCorrect = results[index].correct_answer === answer;
 
     selectedAnswers[index] = { answer: answer, isCorrect: isCorrect };
 
@@ -32,31 +39,30 @@ $(document).ready(function () {
       .addClass("active-answer")
       .siblings()
       .removeClass("active-answer");
-  }
+  };
 
   // Render quiz from the retrieved quiz data
-  function renderQuiz(response) {
+  const renderQuiz = (response) => {
     if (response.results.length) {
       // results = response.results;
       results = [].concat(response.results);
 
       // Loop through the results received from the response and render a quiz question
-      results.forEach(function (quiz, index) {
-        var answers = [quiz.correct_answer]
+      results.forEach((quiz, index) => {
+        const answers = [quiz.correct_answer]
           .concat(quiz.incorrect_answers)
           .sort(() => Math.random() - 0.5);
-
-        // answerList=$('<ul class="answers-list"></ul>');
-        var answerList = document.createElement('ul')
-        answerList.className = 'answers-list';
+        const answerList = $('<ul class="answers-list"></ul>');
+        // const answerList = document.createElement('ul')
+        // answerList.className = 'answers-list';
 
         // Loop through the array of answers and render it
-        answers.forEach(function (answer, index) {
-          // answerList.append('<li class="answer">' + answer + "</li>");
-          var ans = document.createElement('li');
-          ans.className = 'answer';
-          ans.innerHTML = answer;
-          answerList.appendChild(ans);
+        answers.forEach((answer) => {
+          answerList.append('<li class="answer">' + answer + "</li>");
+          // const ans = document.createElement('li');
+          // ans.className = 'answer';
+          // ans.innerHTML = answer;
+          // answerList.appendChild(ans);
         });
 
         // Loop through results and render question and answer
@@ -71,33 +77,55 @@ $(document).ready(function () {
           quiz.question +
           "</h4></li>"
         );
-
         $("#question-" + index + "").append(answerList);
       });
 
       // document.querySelector('.answer').addEventListener('click', onAnswerClick);
       $(".answer").on("click", onAnswerClick);
-      btnGroup.style.display = "flex";
+      btnGroup.css("display", "flex");
     }
-  }
+  };
 
   // fetching quiz data
-  function getQuizDetails() {
-    fetch('https://opentdb.com/api.php?amount=10&category=11&difficulty=easy&type=multiple')
+  const getQuizDetails = () => {
+    fetch("https://opentdb.com/api.php?amount=10&category=11&difficulty=easy&type=multiple")
       .then(res => res.json())
-      .then(renderQuiz);
-  }
+      .then(renderQuiz)
+      .catch(err => alert('\t\t\tSomething went wrong!\n' + err));
+  };
+
+  // // Using async/await
+  // const getQuizDetails = async () => {
+  //   try {
+  //     const res = await fetch("https://opentdb.com/api.php?amount=10&category=11&difficulty=easy&type=multiple")
+  //     alert(res.ok);
+  //     if (!res.ok) {
+  //       throw new Error(res.status);
+  //     }
+  //     const data = await res.json();
+  //     renderQuiz(data);
+  //   }
+  //   catch (error) {
+  //     alert('\t\t\tSomething went wrong!\n' + error);
+  //   }
+  // }
+
 
   // Toggle username modal based on requirement
-  function toggleUsernameModal() {
-    modal.className = 'out';
-  }
+  const toggleUsernameModal = () => {
+    if (modal.hasClass("out")) {
+      modal.removeClass("out");
+    } else {
+      modal.addClass("out");
+    }
+  };
 
-  // accepting and verifying username username
-  function getUsername(event) {
+  // Get the username from modal when user presses enter
+  const getUsername = (event) => {
     if (event.which === 13 || event.keyCode === 13 || event.key === "Enter") {
-      if (usernameInput.value.trim()) {
-        username = usernameInput.value.trim();
+      if (usernameInput.val()) {
+        username = usernameInput.val();
+
         toggleUsernameModal();
         getQuizDetails();
       }
@@ -105,13 +133,13 @@ $(document).ready(function () {
         alert('Please Enter username...');
       }
     }
-  }
+  };
 
   // On submitting quiz answers, run this function
-  function onSubmit(e) {
+  const onSubmit = (e) => {
     e.preventDefault();
 
-    var rightAnswersCount = 0;
+    let rightAnswersCount = 0;
 
     if (Object.keys(selectedAnswers).length === results.length) {
       rightAnswersCount = Object.values(selectedAnswers).filter(
@@ -128,13 +156,17 @@ $(document).ready(function () {
     } else {
       alert("Please answer all questions");
     }
-  }
+  };
 
   // Bind events
-  usernameInput.addEventListener("keypress", getUsername);
-  submitBtn.addEventListener('click', onSubmit);
-  resetBtn.addEventListener('click', function () {
+  usernameInput.on("keypress", getUsername);
+  submitBtn.on("click", onSubmit);
+  resetBtn.on("click", () => {
     location.reload();
   });
-
+  // usernameInput.addEventListener("keypress", getUsername);
+  // submitBtn.addEventListener('click', onSubmit);
+  // resetBtn.addEventListener('click', () => () {
+  //   location.reload();
+  // });
 });
